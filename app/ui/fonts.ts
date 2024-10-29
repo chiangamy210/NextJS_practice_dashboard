5e0650210e90bf2f5379ef9b1a40583f4a75a8f1
Amy <chiangyenhui@gmail.com>
2024-10-29 10:55:52 -0700

    fix some typo for depolyment

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,7 @@
 import { Inter, Lusitana } from "next/font/google";
 export const inter = Inter({ subsets: ["latin"] });
-export const lusitana = Lusitana({ subset: ["latin"], weight: ["400", "700"] });
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  preload: false,
+  weight: ["400", "700"],
+});

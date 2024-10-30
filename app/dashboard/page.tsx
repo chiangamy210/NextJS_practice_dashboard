0dbf82905c91d1c1a7b702d7e9e3b5d3cb807193
Amy <chiangyenhui@gmail.com>
2024-10-29 21:26:29 -0700

    finish chapter 7: use sever component to fetch data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,46 @@
-export default function Page() {
-  return <p>Dashborad Page</p>;
+import { Card } from "../ui/dashboard/cards";
+import RevenueChart from "../ui/dashboard/revenue-chart";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
+import { lusitana } from "../ui/fonts";
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "../lib/data";
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const card = await fetchCardData();
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card
+          title="Collected"
+          value={card.totalPaidInvoices}
+          type="collected"
+        />
+        <Card
+          title="Pending"
+          value={card.totalPendingInvoices}
+          type="pending"
+        />
+        <Card
+          title="Total Invoices"
+          value={card.numberOfInvoices}
+          type="invoices"
+        />
+        <Card
+          title="Total Customers"
+          value={card.numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        {" "}
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
 }

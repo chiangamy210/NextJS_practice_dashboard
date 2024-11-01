06e363b162164caf2c93edfb4cf689b5b9034f8e
Amy <chiangyenhui@gmail.com>
2024-11-01 11:43:57 -0700

    fix some type compatible error for deployment

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -13,7 +13,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
 
   const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
-    params.set("page", pageNumber);
+    params.set("page", pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   };
   const allPages = generatePagination(currentPage, totalPages);
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,7 +3,11 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
-export default function Search({ placeholder }) {
+export default function Search({
+  placeholder = "Search",
+}: {
+  placeholder?: string;
+}) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();

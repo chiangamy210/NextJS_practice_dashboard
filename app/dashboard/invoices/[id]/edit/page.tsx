6727baa4a387b0f3729ad5a7bb2a912c7ee5a337
Amy <chiangyenhui@gmail.com>
2024-11-07 21:55:15 -0800

    finish chapter 16: setting metadata to improve SEO

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,11 @@ import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "edit invoices ",
+};
 export default async function page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const id = params.id;
@@ -30,3 +34,4 @@ export default async function page(props: { params: Promise<{ id: string }> }) {
     </main>
   );
 }
+//TODO fix run build
--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,7 +1,11 @@
 import Form from "../../../ui/invoices/create-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers } from "@/app/lib/data";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "create invoices",
+};
 export default async function page() {
   const customers = await fetchCustomers();
 
